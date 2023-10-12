214efe5c6056f0403638fab6d3d6b59f9cec38ea
Brennez <tchalisantos40@gmail.com>
2023-10-12 16:58:38 -0300

    refact: modificando o componente de app bar para utilizar em outras telas

--- dart/lib/components/app_bar_component.dart
+++ dart/lib/components/app_bar_component.dart
@@ -1,49 +1,60 @@
 import 'package:flutter/material.dart';
 
 class AppBarComponent extends StatelessWidget {
-  const AppBarComponent({super.key});
+  final bool subtitleOn;
+  AppBarComponent({super.key, this.subtitleOn = true});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Chuva',
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Icon(
-                Icons.favorite,
-                color: Color(0xffb642f5),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Flutter',
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            'Programação',
-            style: TextStyle(
-              color: Colors.white,
+    return AppBar(
+      toolbarHeight: 100,
+      backgroundColor: const Color(0xff456189),
+      centerTitle: true,
+      title: SafeArea(
+        child: Column(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Chuva',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.favorite,
+                  color: Color(0xffb642f5),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Flutter',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w600),
+                ),
+              ],
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 5,
+            ),
+            if (subtitleOn)
+              const Text(
+                'Programação',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+          ],
+        ),
+      ),
+      leading: const Icon(
+        Icons.arrow_back_ios,
+        color: Colors.white,
       ),
     );
   }
