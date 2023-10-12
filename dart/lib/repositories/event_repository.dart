309f3859a77d47b6e140f831ae1df1882901b1cc
Brennez <tchalisantos40@gmail.com>
2023-10-12 10:29:33 -0300

    refactor: fazendo requisições nos dois arquivos JSON e retornando uma unica base de dados para ser consumida

--- dart/lib/repositories/event_repository.dart
+++ dart/lib/repositories/event_repository.dart
@@ -17,26 +17,43 @@ class EventRepository implements IEventRepository {
 
   @override
   Future<List<EventModel>> getEvents() async {
-    final response = await client.get(
+    final response1 = await client.get(
         url:
             'https://raw.githubusercontent.com/chuva-inc/exercicios-2023/master/dart/assets/activities.json');
 
-    if (response.statusCode == 200) {
-      // print('AQUI: ${response.body}');
-      List<EventModel> items = [];
+    final response2 = await client.get(
+        url:
+            'https://raw.githubusercontent.com/chuva-inc/exercicios-2023/master/dart/assets/activities-1.json');
 
-      final body = jsonDecode(response.body);
+    if (response1.statusCode == 200 && response2.statusCode == 200) {
+      final mergedItems = _responsesToListConverter(response1, response2);
 
-      body['data'].map((item) {
-        final eventModel = EventModel.fromJson(item);
-        items.add(eventModel);
-      }).toList();
-
-      return items;
-    } else if (response.statusCode == 404) {
+      return mergedItems;
+    } else if (response1.statusCode == 404 || response2.statusCode == 404) {
       throw NotFoundError(message: 'Url não encontrada');
     } else {
       throw Exception('Erro inesperado');
     }
+  }
+
+  List<EventModel> _responsesToListConverter(
+      dynamic response1, dynamic response2) {
+    List<EventModel> items = [];
+
+    final body1 = jsonDecode(response1.body);
+
+    final body2 = jsonDecode(response2.body);
+
+    body1['data'].map((item) {
+      final eventModel = EventModel.fromJson(item);
+      items.add(eventModel);
+    }).toList();
+
+    body2['data'].map((item) {
+      final eventModel = EventModel.fromJson(item);
+      items.add(eventModel);
+    }).toList();
+
+    return items;
   }
 }
