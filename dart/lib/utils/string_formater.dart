cd84dd286dbdf5edc49c63e5672cc3470dab464f
Brennez <tchalisantos40@gmail.com>
2023-10-13 23:06:41 -0300

    feat: funcao que retorna as iniciais do nome

--- dart/lib/utils/string_formater.dart
+++ dart/lib/utils/string_formater.dart
@@ -2,4 +2,13 @@ class StringFormater {
   static removeHtmlTags(String text) {
     return text.replaceAll(RegExp(r'<[^>]*>'), '');
   }
+
+  static getInitialName(List<dynamic> name) {
+    List<String> separeteName = name[0].split(" ");
+
+    String firstLetter = separeteName[0][0];
+    String secondtLetter = separeteName[1][0];
+
+    return firstLetter + secondtLetter;
+  }
 }
